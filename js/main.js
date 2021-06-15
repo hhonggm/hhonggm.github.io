1acdf844fa64ed65c17beedf23b050cb6727df22
gonet <gonet@gonetui-MacBookPro.local>
2021-06-15 22:02:47 +0900

    js update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(function() {
     $(window).on("scroll",function(){
-    
+        var scrollTop = $(window).scrollTop();
+        var offsetTop = $("#projects .box_wrap").offset().top;
+        var height = $("#projects .box_wrap").height();
+        var offsetBottom = offsetTop + height;
+        var thisHeight = $("#projects .box_wrap .list1").height();
+        var abBottom = offsetBottom - thisHeight;
+
+        if(offsetTop < scrollTop && scrollTop <= abBottom){
+            $("#projects .box_wrap").addClass("fix");
+        }else if(abBottom < scrollTop){
+            $("#projects .box_wrap").addClass("bottom");
+            $("#projects .box_wrap").removeClass("fix");
+        }else{
+            $("#projects .box_wrap").removeClass("fix");
+            $("#projects .box_wrap").removeClass("bottom");
+        }
     });
 
     $(document).on('click','#gnb_layout ul li a',function(){
@@ -13,7 +28,7 @@ $(function() {
 
 
     visualTextshuffle();
-    AOS.init();
+//     AOS.init();
     getProject();
 });
 
@@ -26,11 +41,14 @@ function getProject(){
             // console.log(item);
 
             var itemHTML = "";
-            for(var i = 0; i < item.length; i++){
-                itemHTML += '<div class="box" data-aos="zoom-out-up">';
+            for(var i = 1; i < item.length; i++){
+                itemHTML += '<div class="box">';
                 // console.log("item.type"+item[i].type);
                 if(item[i].type == "link") {
-                    itemHTML += '<a class="link" href="' + item[i].linkUrl + '" title="새창열림" target="_blank"  style="background-image: url('+ item[i].imgUrl +')">';
+                    itemHTML += '<a class="link" href="' + item[i].linkUrl + '" title="새창열림" target="_blank">';
+                    itemHTML += '<div class="thumb">';
+                    itemHTML += '<img src="'+ item[i].imgUrl +'" alt="">';
+                    itemHTML += '</div>';
                     itemHTML += '<div class="txt">';
                     itemHTML += '<span class="label">'+ item[i].label +'</span>';
                     itemHTML += '<strong>'+ item[i].title +'</strong>';
@@ -40,7 +58,10 @@ function getProject(){
                     itemHTML += '</div>';
                     itemHTML += '</a>';
                 }else if(item[i].type == "modal"){
-                    itemHTML += '<button  style="background-image: url('+ item[i].imgUrl +')" type="button" class="link" data-button="modal" data-target="'+ item[i].target +'" data-width="auto">';
+                    itemHTML += '<button type="button" class="link" data-button="modal" data-target="'+ item[i].target +'" data-width="auto">';
+                    itemHTML += '<div class="thumb">';
+                    itemHTML += '<img src="'+ item[i].imgUrl +'" alt="">';
+                    itemHTML += '</div>';
                         itemHTML += '<div class="txt">';
                             itemHTML += '<span class="label type1">'+ item[i].label +'</span>';
                             itemHTML += '<strong>'+ item[i].title +'</strong>';
@@ -53,7 +74,41 @@ function getProject(){
                 itemHTML += '</div>';
 
             }
+            
+            var itemHTML2 = "";
+            itemHTML2 += '<div class="box">';
+            // console.log("item.type"+item[i].type);
+            if(item[0].type == "link") {
+                itemHTML2 += '<a class="link" href="' + item[0].linkUrl + '" title="새창열림" target="_blank">';
+                itemHTML2 += '<div class="thumb">';
+                itemHTML2 += '<img src="'+ item[0].imgUrl +'" alt="">';
+                itemHTML2 += '</div>';
+                itemHTML2 += '<div class="txt">';
+                itemHTML2 += '<span class="label">'+ item[0].label +'<b>01</b></span>';
+                itemHTML2 += '<strong>'+ item[0].title +'</strong>';
+                itemHTML2 += '<ul>';
+                itemHTML2 += '</ul>';
+                itemHTML2 += '<em>VIEW</em>';
+                itemHTML2 += '</div>';
+                itemHTML2 += '</a>';
+            }else if(item[0].type == "modal"){
+                itemHTML2 += '<button type="button" class="link" data-button="modal" data-target="'+ item[0].target +'" data-width="auto">';
+                itemHTML2 += '<div class="thumb">';
+                itemHTML2 += '<img src="'+ item[0].imgUrl +'" alt="">';
+                itemHTML2 += '</div>';
+                itemHTML2 += '<div class="txt">';
+                        itemHTML2 += '<span class="label type1">'+ item[0].label +'<b>01</b></span>';
+                        itemHTML2 += '<strong>'+ item[0].title +'</strong>';
+                        itemHTML2 += '<ul>';
+                        itemHTML2 += '</ul>';
+                        itemHTML2 += '<em>VIEW</em>';
+                    itemHTML2 += '</div>';
+                itemHTML2 += '</button>';
+            }
+            itemHTML2 += '</div>';
+
             $('#projects .box_wrap .list2').html(itemHTML);
+            $('#projects .box_wrap .list1').html(itemHTML2);
         }
     });
 }
